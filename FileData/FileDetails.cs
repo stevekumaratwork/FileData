e14bec84c7821db917f0a1b0a80bc7e266389a4c
Kumar-PC\Kumar <stevekumaratwork@gmail.com>
2018-10-06 14:43:10 +0100

    Add project files.

---
+++ FileData/FileDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileData
+{
+    public class FileDetails
+    {
+        public decimal Version { get; set; }
+        public decimal Size { get; set; }
+        public string Argument { get; set; }
+
+    }
+}
---
+++ FileData/OutputStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileData
+{
+    public enum OutputStatus
+    {
+        ValidArgument,
+        InvalidArgument,
+        Unknown
+
+    }
+}
